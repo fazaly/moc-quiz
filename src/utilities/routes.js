4f881baf184f5ac9a7376ae25f4cc0c657dec00f
Md. Fazaly Rabby <fazalyrabbi614@gmail.com>
2022-10-11 09:23:47 +0600

    blog completed

--- src/utilities/routes.js
+++ src/utilities/routes.js
@@ -1,11 +1,37 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from '../components/Home/Home';
+import Topics from '../components/Topics/Topics';
+import Statistics from '../components/Statistics/Statistics';
+import Blog from '../components/Blog/Blog';
 import Root from "../components/Root/Root";
 
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Root></Root>
+        element: <Root></Root>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/home',
+                element: <Home></Home>
+            },
+            {
+                path: '/topics',
+                element: <Topics></Topics>
+            },
+            {
+                path: '/statistics',
+                element: <Statistics></Statistics>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            }
+        ]
     }
 ])
 
